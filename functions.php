af56b21881af83cbb9b0f91212bd4f04775d277d
Andy Field <andy.field@starleaf.com>
2019-11-05 11:28:40 +0000

    Updated functions.php

--- functions.php
+++ functions.php
@@ -11,29 +11,46 @@
 
 // Remove actions
 
-remove_action('wp_head', 'feed_links_extra', 3); // Display the links to the extra feeds such as category feeds
-remove_action('wp_head', 'feed_links', 2); // Display the links to the general feeds: Post and Comment Feed
-remove_action('wp_head', 'rsd_link'); // Display the link to the Really Simple Discovery service endpoint, EditURI link
-remove_action('wp_head', 'wlwmanifest_link'); // Display the link to the Windows Live Writer manifest file.
-remove_action('wp_head', 'index_rel_link'); // Index link
-remove_action('wp_head', 'parent_post_rel_link', 10, 0); // Prev link
-remove_action('wp_head', 'start_post_rel_link', 10, 0); // Start link
-remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0); // Display relational links for the posts adjacent to the current post.
-remove_action('wp_head', 'wp_generator'); // Display the XHTML generator that is generated on the wp_head hook, WP version
-remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
-remove_action('wp_head', 'rel_canonical');
-remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+remove_action( 'wp_head', 'wlwmanifest_link' ); //  Remove link to Windows Live Writer manifest file <link rel="wlwmanifest" type="application/wlwmanifest+xml">
+remove_action( 'wp_head', 'wp_generator' ); // Remove <meta name="generator" content="WordPress {version}">
+remove_action( 'wp_head', 'rsd_link' ); // Remove the link to the Really Simple Discovery service endpoint, EditURI link
+remove_action( 'wp_head', 'wp_shortlink_wp_head' ); // Remove the shortlink
+remove_action( 'wp_head', 'rest_output_link_wp_head', 10 ); // Remove api.w.org relation links
+remove_action( 'wp_head', 'wp_oembed_add_discovery_links', 10 ); // Remove api.w.org relation links
+remove_action( 'template_redirect', 'rest_output_link_header', 11, 0 ); // Remove api.w.org relation links
+remove_action( 'wp_head', 'feed_links', 2 ); // Remove the links to the general feeds: Post and Comment Feed
+remove_action( 'wp_head', 'feed_links_extra', 3 ); // Remove the links to the extra feeds such as category feeds
+remove_action( 'wp_head', 'index_rel_link' ); // Remove the index post relational link
+remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 ); // Remove the previous post relational link
+remove_action( 'wp_head', 'start_post_rel_link', 10, 0 ); // Remove the start post relational link
+remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 ); // Remove the adjacent post relational links
+// remove_action('wp_head', 'rel_canonical'); // Remove the canonical link
+
+// Disable emojis
+
 remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 remove_action( 'wp_print_styles', 'print_emoji_styles' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
+remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+add_filter( 'emoji_svg_url', function(){ return false; } );
 
-// Add filters
+// Disable smilies
 
-add_filter('widget_text', 'do_shortcode'); // Allow shortcodes in the sidebar
-add_filter('widget_text', 'shortcode_unautop'); // Remove <p> tags from sidebars
+register_activation_hook( __FILE__, function(){ update_option( 'use_smilies', false ); } );
+register_deactivation_hook( __FILE__, function(){ update_option( 'use_smilies', true ); } );
 
-// Remove filters
+// Sidebars
 
-remove_filter('the_excerpt', 'wpautop'); // Remove <p> tags from the excerpt
+add_filter( 'widget_text', 'do_shortcode' ); // Allow shortcodes in the sidebar
+add_filter( 'widget_text', 'shortcode_unautop' ); // Remove <p> tags from sidebars
+
+// Disable auto insertion of <p> tags
+
+remove_filter( 'the_excerpt', 'wpautop' );
+// remove_filter( 'the_content', 'wpautop' );
 
 // Menus
 
@@ -59,7 +76,7 @@ function remove_thumbnail_dimensions( $html, $post_id, $post_image_id ) {
 
 // Sidebars
 
-if (function_exists('cassiopeia_sidebar')) {
+if (function_exists( 'cassiopeia_sidebar' )) {
     register_sidebar(array(
         'name' => __('Sidebar Widget 1', 'cassiopeia'),
         'description' => __('Description for this widget', 'cassiopeia'),
@@ -70,6 +87,31 @@ if (function_exists('cassiopeia_sidebar')) {
         'after_title'  =>  '</h3>'
     ));
 }
+
+// Remove Gutenberg block CSS
+
+function disable_gutenberg_block_css() {
+    wp_dequeue_style( 'wp-block-library' );
+}
+add_action( 'wp_enqueue_scripts', 'disable_gutenberg_block_css', 999 );
+
+// Replace "Powered by Wordpress" H1 on login page
+
+add_filter( 'login_headerurl', function(){ return home_url(); });
+add_filter( 'login_headertext', function(){ return get_bloginfo( 'name', 'display' ); });
+
+// Send system emails from admin email and blog name
+
+add_filter( 'wp_mail_from', function($email){
+    if( substr($email,0,10) === 'wordpress@')
+        $email = get_option('admin_email');
+    return $email;
+});
+add_filter( 'wp_mail_from_name', function($name){
+    if($name === 'WordPress')
+        $name = str_replace( '&#039;', "'", get_option('blogname') );
+    return $name;
+});
 
 // Pagination for paged posts. Includes pages and next and previous links.
 
